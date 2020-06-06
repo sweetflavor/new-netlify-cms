bbf7dc20b6d26b4058456a253823e1d9dd55efa1
Alex Man <sweet.devtools@gmail.com>
2020-06-06 18:45:25 +0200

    com

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        netlify-cms-app
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+   <h1>Hola</h1>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
 }
 </script>
 
